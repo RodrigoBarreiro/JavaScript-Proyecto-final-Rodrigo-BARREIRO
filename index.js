5397c455c1f6c2f55a68b8b2ab66d7f93d490f1e
RodrigoBarreiro <barreiro.rb@gmail.com>
2022-09-05 20:52:25 -0300

    se agregaron arreglos, descuentos y total carrito

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ class Producto{
     }
     vender(cantidad){
         if ( this.stock - cantidad < 0){
-            console.log ("No suficientes productos para la cantidad solicitada");
+            console.log ("No hay suficientes productos para la cantidad solicitada");
         }else{
             this.stock = this.stock - cantidad;
         }
@@ -30,10 +30,10 @@ const cerveza1 = new Producto ("Witbier", 300, "1 lts", 10 );
 const cerveza2 = new Producto ("Pale Ale", 450,"1 lts", 20 );
 const cerveza3 = new Producto ("Wite IPA", 400, "1 lts", 40 );
 const cerveza4 = new Producto ("Dry Stout", 300,"1 lts", 12 );
-const cerveza5 = new Producto ("Wite IPA", 300, "1 lts", 10 );
-const cerveza6 = new Producto ("Wite IPA", 300, "1 lts", 10 );
-const cerveza7 = new Producto ("Wite IPA", 300, "1 lts", 10 );
-const cerveza8 = new Producto ("Wite IPA", 300, "1 lts", 10 );
+const cerveza5 = new Producto ("Wite IPA", 290, "1 lts", 30 );
+const cerveza6 = new Producto ("Wite IPA", 380, "1 lts", 20 );
+const cerveza7 = new Producto ("Wite IPA", 380, "1 lts", 12 );
+const cerveza8 = new Producto ("Wite IPA", 450, "1 lts", 15 );
 console.log (cerveza1);
 console.log (cerveza2);
 cerveza1.vender(11);
@@ -41,7 +41,7 @@ cerveza2.vender(1);
 console.log (cerveza1);
 console.log (cerveza2);
 
-
+// PRECIO TOTAL A ABONAR
 const suma = (a, b) => a + b;
 const resta = (a, b) => a - b;
 const iva = x => x * 0.21;
@@ -49,4 +49,39 @@ let precioProducto = 400;
 let descuento = 100;
 
 let precioFinal = resta (suma (precioProducto, iva (precioProducto)), descuento);
-console.log ( "El precio abonar es de " + precioFinal);
+console.log ( "El precio abonar es de " + precioFinal); 
+
+// ARREGLO CON CERVEZAS
+const cervezas = [
+    {id: 1, nombre: "Witbier", precio: 300, envase: "1 lts", stock: 10 },
+    {id: 2, nombre: "Pale Ale", precio: 450, envase: "1 lts", stock: 20 },
+    {id: 3, nombre: "Wite IPA", precio: 400, envase: "1 lts", stock: 40 },
+    {id: 4, nombre: "Dry Stout", precio: 300, envase: "1 lts", stock: 12 },
+    {id: 5, nombre: "K7 Doble IPA", precio: 290, envase: "1 lts", stock: 30 },
+    {id: 6, nombre: "174 IPA", precio: 380, envase: "1 lts", stock: 20 },
+    {id: 7, nombre: "Garaje Brown ALE", precio: 380, envase: "1 lts", stock: 12 },
+    {id: 8, nombre: "LIMA pale ALE", precio: 450, envase: "1 lts", stock: 15 },
+]
+
+console.log(cervezas);
+
+// PROMOCION DESCUENTO DE IVA PARA CERVEZAS
+let descuentoProductos = cervezas.map (item => {
+    return {
+        id: item.id,
+        nombre: item.nombre,
+        precio: item.precio - item.precio * 0.21,
+    }
+})
+
+console.log(descuentoProductos);
+
+// TOTAL CARRITO
+const carrito = [
+    {id: 1, nombre: "Witbier", precio: 300, envase: "1 lts" },
+    {id: 2, nombre: "Pale Ale", precio: 450, envase: "1 lts" },
+    {id: 3, nombre: "Wite IPA", precio: 400, envase: "1 lts" },
+];
+
+let total = carrito.reduce (( acumulador, item) => acumulador + item.precio, 0);
+console.log("Usted debe abonar un total de " + total);
